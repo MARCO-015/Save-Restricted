2097ce473914a715b96f15f365c9b1d7ac484ca6
MARCO-015 <Charlie.aimers07@gmail.com>
2025-02-02 17:49:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7784502607:AAFd5S3PFFTep5I8a8CAjzPFTrdKt8T7RYI"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7548265642").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Test2:Monstersir123@cluster0.ltadb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002275877038")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "2341507214"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002341507214"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "modijiurl.com")
